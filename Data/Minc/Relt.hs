6cfc1c70da20478675cf9ce6bb048620e64c73e7
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2015-03-03 15:54:02 +1000

    new files

---
+++ Data/Minc/Relt.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE MagicHash, UnboxedTuples, TypeSynonymInstances, FlexibleInstances #-}
+
+module Data.Minc.Relt where
+
+import GHC.Prim
+import GHC.Exts
+import GHC.Types
+import GHC.Word
+import GHC.Int
+
+import Data.Array.Repa.Eval
+import Foreign.C.Types (CDouble(..))
+
+-- instance Elt CDouble
+-- instance Elt CDouble
